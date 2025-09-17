ad7514bfc452d4061d30e61022fba91695ffa893
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-09-17 21:06:58 +0200

    test: add a test case that will ensure that the 3D sat works (it fails ATM)

--- test/Extensions/PhysicsEngine/RigidBodies/Rigidbody.test.ts
+++ test/Extensions/PhysicsEngine/RigidBodies/Rigidbody.test.ts
@@ -93,6 +93,64 @@ describe("Rigidbody", (): void => {
   });
 
   /**
+   * Tests if the rigidbody bounce without falling through the collider
+   */
+  it("should not bounce", () => {
+    // Rigidbody
+    const object1: GameObject = new GameObject("Object1");
+    gameEngineWindow.root.addChild(object1);
+    const vertices1: Vector3[] = [
+      new Vector3(1, 2, 1),
+      new Vector3(1, 0, 1),
+      new Vector3(-1, 0, -1),
+      new Vector3(-1, 2, -1),
+    ];
+    const polygonCollider1: PolygonCollider = new PolygonCollider(vertices1);
+    const rigidBody1 = new Rigidbody(polygonCollider1, 1, 1);
+    object1.addBehavior(polygonCollider1);
+    object1.addBehavior(rigidBody1);
+
+    // Collider
+    const object2: GameObject = new GameObject("Object2");
+    gameEngineWindow.root.addChild(object2);
+    const vertices2: Vector3[] = [
+      new Vector3(-1, 0, 1.2),
+      new Vector3(1, 0, 1.2),
+    ];
+    const polygonCollider2: PolygonCollider = new PolygonCollider(vertices2);
+    object2.addBehavior(polygonCollider2);
+    object2.transform.position.set(0, -4.905, 0);
+
+    gameEngineWindow.addGameComponent(physicsGameEngineComponent);
+    // check position at s0
+    expect(object2.transform.position.x).toBe(0);
+    expect(object2.transform.position.y).toBe(-4.905);
+    expect(object2.transform.position.z).toBe(0);
+    expect(object1.transform.position.x).toBe(0);
+    expect(object1.transform.position.y).toBe(0);
+    expect(object1.transform.position.z).toBe(0);
+    expect(rigidBody1.linearVelocity).toStrictEqual(new Vector3(0, 0, 0));
+
+    // Check position at s1
+    manualTicker.tick(1);
+    expect(rigidBody1.linearVelocity.x).toBe(0);
+    expect(rigidBody1.linearVelocity.y).toBeCloseTo(-9.81);
+    expect(rigidBody1.linearVelocity.z).toBe(0);
+    expect(object1.transform.position.x).toBe(0);
+    expect(object1.transform.position.y).toBeCloseTo(-4.905);
+    expect(object1.transform.position.z).toBe(0);
+
+    // Check position at s2
+    manualTicker.tick(1);
+    expect(rigidBody1.linearVelocity.x).toBe(0);
+    expect(rigidBody1.linearVelocity.y).toBeCloseTo(-19.62);
+    expect(rigidBody1.linearVelocity.z).toBe(0);
+    expect(object1.transform.position.x).toBe(0);
+    expect(object1.transform.position.y).toBeCloseTo(-19.62);
+    expect(object1.transform.position.z).toBe(0);
+  });
+
+  /**
    * Tests if the bounce on a collider works well with full restitution
    */
   it("should bounce on collider with full resitution", () => {

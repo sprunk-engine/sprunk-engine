9154093e65bda2f920a0cb3b7cae8385a77b4078
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-09-17 20:18:43 +0200

    refactor(test): add 3rd dimention complexity to the PolygonCollider tests

--- test/Extensions/PhysicsEngine/PolygonCollider.test.ts
+++ test/Extensions/PhysicsEngine/PolygonCollider.test.ts
@@ -2,7 +2,6 @@ import { describe, it, expect, beforeEach } from "vitest";
 import { PolygonCollider } from "@extensions/PhysicsEngine/Colliders/PolygonCollider.ts";
 import { Vector3 } from "@core/MathStructures/Vector3.ts";
 import { GameObject } from "@core/GameObject.ts";
-import { Quaternion } from "@core/MathStructures/Quaternion";
 
 describe("PolygonCollider", (): void => {
   let vertices: Vector3[];
@@ -11,9 +10,9 @@ describe("PolygonCollider", (): void => {
 
   beforeEach(() => {
     vertices = [
-      new Vector3(1, 2, 0),
-      new Vector3(3, 4, 0),
-      new Vector3(5, 6, 0),
+      new Vector3(1, 2, 3),
+      new Vector3(3, 4, 5),
+      new Vector3(5, 6, 7),
     ];
     gameObject = new GameObject();
     polygonCollider = new PolygonCollider(vertices);
@@ -24,14 +23,14 @@ describe("PolygonCollider", (): void => {
    * Tests if a polygonCollider can be successfully scaled.
    */
   it("should get a scaled polygonCollider without changing the original", () => {
-    gameObject.transform.scale.set(2, 3, 1);
+    gameObject.transform.scale.set(2, 3, 4);
 
     const transformedPolygonCollider =
       polygonCollider.getVerticesWithTransform();
 
-    expect(transformedPolygonCollider[0]).toEqual(new Vector3(2, 6, 0));
-    expect(transformedPolygonCollider[1]).toEqual(new Vector3(6, 12, 0));
-    expect(transformedPolygonCollider[2]).toEqual(new Vector3(10, 18, 0));
+    expect(transformedPolygonCollider[0]).toEqual(new Vector3(2, 6, 12));
+    expect(transformedPolygonCollider[1]).toEqual(new Vector3(6, 12, 20));
+    expect(transformedPolygonCollider[2]).toEqual(new Vector3(10, 18, 28));
     expect(polygonCollider.vertices[0]).toBe(vertices[0]);
     expect(polygonCollider.vertices[1]).toBe(vertices[1]);
     expect(polygonCollider.vertices[2]).toBe(vertices[2]);
@@ -48,10 +47,16 @@ describe("PolygonCollider", (): void => {
 
     expect(transformedPolygonCollider[0].x).toBeCloseTo(-2);
     expect(transformedPolygonCollider[0].y).toBeCloseTo(1);
+    expect(transformedPolygonCollider[0].z).toBeCloseTo(3);
+
     expect(transformedPolygonCollider[1].x).toBeCloseTo(-4);
     expect(transformedPolygonCollider[1].y).toBeCloseTo(3);
+    expect(transformedPolygonCollider[1].z).toBeCloseTo(5);
+
     expect(transformedPolygonCollider[2].x).toBeCloseTo(-6);
     expect(transformedPolygonCollider[2].y).toBeCloseTo(5);
+    expect(transformedPolygonCollider[2].z).toBeCloseTo(7);
+
     expect(polygonCollider.vertices[0]).toBe(vertices[0]);
     expect(polygonCollider.vertices[1]).toBe(vertices[1]);
     expect(polygonCollider.vertices[2]).toBe(vertices[2]);
@@ -61,7 +66,7 @@ describe("PolygonCollider", (): void => {
    * Tests if a polygonCollider can be successfully translated.
    */
   it("should get a translated polygonCollider without changing the original", () => {
-    gameObject.transform.position.set(1, 2, 0);
+    gameObject.transform.position.set(1, 2, 3);
 
     const transformedPolygonCollider =
       polygonCollider.getVerticesWithTransform();
@@ -69,14 +74,8 @@ describe("PolygonCollider", (): void => {
     expect(polygonCollider.vertices[0]).toBe(vertices[0]);
     expect(polygonCollider.vertices[1]).toBe(vertices[1]);
     expect(polygonCollider.vertices[2]).toBe(vertices[2]);
-    expect(transformedPolygonCollider[0]).toEqual(
-      vertices[0].add(gameObject.transform.position),
-    );
-    expect(transformedPolygonCollider[1]).toEqual(
-      vertices[1].add(gameObject.transform.position),
-    );
-    expect(transformedPolygonCollider[2]).toEqual(
-      vertices[2].add(gameObject.transform.position),
-    );
+    expect(transformedPolygonCollider[0]).toEqual(new Vector3(2, 4, 6));
+    expect(transformedPolygonCollider[1]).toEqual(new Vector3(4, 6, 8));
+    expect(transformedPolygonCollider[2]).toEqual(new Vector3(6, 8, 10));
   });
 });

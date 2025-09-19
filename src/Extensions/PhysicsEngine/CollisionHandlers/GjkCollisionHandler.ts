df94862ce85e5d749af71f3b74c26d01184004cf
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-09-19 12:32:29 +0200

    feat: add the first support point in the GJK algo

--- src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
+++ src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
@@ -1,12 +1,26 @@
-import { CollisionHandler } from "./CollisionHandler";
+import { CollisionHandler } from "@extensions/PhysicsEngine/CollisionHandlers/CollisionHandler.ts";
 import { ShapedCollider } from "@extensions/PhysicsEngine/Colliders/ShapedCollider.ts";
 import { Collision } from "@extensions/PhysicsEngine/Colliders/Collision.ts";
+import { Vector3 } from "@core/MathStructures/Vector3.ts";
 
 export class GjkCollisionHandler implements CollisionHandler {
+  private simplex: Vector3[] = [];
+
   areColliding(a: ShapedCollider, b: ShapedCollider): Collision | null {
-    const centerA = a.getGravitationCenter();
-    const centerB = b.getGravitationCenter();
+    const centerA = a.getGravitationCenter().add(a.getWorldPosition());
+    const centerB = b.getGravitationCenter().add(b.getWorldPosition());
+
+    // Set initial support point
+    let d = centerB.clone().sub(centerA); // Direction from A to B
+    this.simplex.push(this.support(a, b, d)); // Initial support point
 
     return null; // Placeholder return
+  }
+
+  // Get the support point in the Minkowski Difference
+  private support(a: ShapedCollider, b: ShapedCollider, d: Vector3): Vector3 {
+    const pointA = a.getSupportPoint(d).add(a.getWorldPosition());
+    const pointB = b.getSupportPoint(d.scale(-1)).add(b.getWorldPosition());
+    return pointA.sub(pointB);
   }
 }

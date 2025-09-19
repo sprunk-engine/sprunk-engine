d4624ff1e6b370490f0f5124585abf226d69989e
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-09-19 11:26:52 +0200

    feat: add a new collision handler that will use the GJK algo to optimize the collision detection to a O(log n)

---
+++ src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
@@ -0,0 +1,9 @@
+import { CollisionHandler } from "./CollisionHandler";
+import { Collider } from "@extensions/PhysicsEngine/Colliders/Collider.ts";
+import { Collision } from "@extensions/PhysicsEngine/Colliders/Collision.ts";
+
+export class SatCollisionHandler implements CollisionHandler {
+  areColliding(a: Collider, b: Collider): Collision | null {
+    return null; // Placeholder return
+  }
+}

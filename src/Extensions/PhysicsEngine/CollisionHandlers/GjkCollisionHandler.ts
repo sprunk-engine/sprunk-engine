4f56ddde0956ffcbeccfb300d1a4ae9ed60f275b
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-09-19 11:34:07 +0200

    fix: typo in GJK class name

--- src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
+++ src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
@@ -2,7 +2,7 @@ import { CollisionHandler } from "./CollisionHandler";
 import { Collider } from "@extensions/PhysicsEngine/Colliders/Collider.ts";
 import { Collision } from "@extensions/PhysicsEngine/Colliders/Collision.ts";
 
-export class SatCollisionHandler implements CollisionHandler {
+export class GjkCollisionHandler implements CollisionHandler {
   areColliding(a: Collider, b: Collider): Collision | null {
     return null; // Placeholder return
   }

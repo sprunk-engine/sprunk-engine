3ce99bf0f8d5b38f89d4503e8d8def9f4c4bba02
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-09-21 21:31:49 +0200

    refactor: use the last point of the simplex by default in the didSupportPassOrigin

--- src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
+++ src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
@@ -17,14 +17,19 @@ export class GjkCollisionHandler implements CollisionHandler {
     // Set second support point
     d = d.scale(-1); // Reverse direction (passing through the origin)
     this.simplex.push(this.support(a, b, d));
-    if (!this.didSupportPassOrigin(this.simplex[1]!, d)) {
+    if (!this.didSupportPassOrigin(d)) {
       return null; // No collision
     }
 
     return null; // Placeholder return
   }
 
-  // Get the support point in the Minkowski Difference
+  /**
+   * Get the support point in the Minkowski Difference
+   * @param a First collider
+   * @param b Second collider
+   * @param d Direction vector
+   */
   private support(a: ShapedCollider, b: ShapedCollider, d: Vector3): Vector3 {
     const pointA = a.getSupportPoint(d).add(a.getWorldPosition());
     const pointB = b.getSupportPoint(d.scale(-1)).add(b.getWorldPosition());
@@ -33,10 +38,12 @@ export class GjkCollisionHandler implements CollisionHandler {
 
   /**
    * Checks if the support points have passed the origin
-   * @param point Support point
    * @param d direction vector
    */
-  private didSupportPassOrigin(point: Vector3, d: Vector3): boolean {
-    return point.dotProduct(d) >= 0;
+  private didSupportPassOrigin(d: Vector3): boolean {
+    const lastIndex = this.simplex.length - 1;
+    const lastPoint = this.simplex[lastIndex];
+    if (!lastPoint) throw new Error("Simplex is empty");
+    return lastPoint.dotProduct(d) >= 0;
   }
 }

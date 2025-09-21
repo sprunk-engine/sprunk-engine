c94f3c1542e8dfd7aa8642f5c29910d4da7fc977
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-09-21 22:14:36 +0200

    feat: add a method getSimplexEdge to compute the new directions usings triple cross products

--- src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
+++ src/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts
@@ -4,7 +4,8 @@ import { Collision } from "@extensions/PhysicsEngine/Colliders/Collision.ts";
 import { Vector3 } from "@core/MathStructures/Vector3.ts";
 
 export class GjkCollisionHandler implements CollisionHandler {
-  private simplex: [Vector3?, Vector3?, Vector3?, Vector3?] = [];
+  private simplex: [Vector3?, Vector3?, Vector3?, Vector3?] = []; // higher the index, younger the point
+  private readonly origin = new Vector3(0, 0, 0);
 
   areColliding(a: ShapedCollider, b: ShapedCollider): Collision | null {
     const centerA = a.getGravitationCenter().add(a.getWorldPosition());
@@ -20,6 +21,12 @@ export class GjkCollisionHandler implements CollisionHandler {
     if (!this.didSupportPassOrigin(d)) {
       return null; // No collision
     }
+
+    // Set third support point
+    let ba = this.getSimplexEdge(0, 1); // Edge from B to A
+    let oa = this.simplex[1]!.scale(-1); // Vector from A to Origin
+    d = ba.crossProduct(oa).crossProduct(ba);
+    this.simplex.push(this.support(a, b, d));
 
     return null; // Placeholder return
   }
@@ -45,5 +52,15 @@ export class GjkCollisionHandler implements CollisionHandler {
     const lastPoint = this.simplex[lastIndex];
     if (!lastPoint) throw new Error("Simplex is empty");
     return lastPoint.dotProduct(d) >= 0;
+  }
+
+  /**
+   * Get the edge vector between two points in the simplex
+   */
+  private getSimplexEdge(index1: number, index2: number): Vector3 {
+    const point1 = this.simplex[index1];
+    const point2 = this.simplex[index2];
+    if (!point1 || !point2) throw new Error("Simplex is incomplete");
+    return point2.sub(point1);
   }
 }

756248f1d60eaa6a801ca7c3c7a585ccfc753d47
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-09-22 20:45:13 +0200

    test: add a case to ensure that GJK does not trigger fake collisions

--- test/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.test.ts
+++ test/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.test.ts
@@ -7,6 +7,7 @@ import { GameObject } from "@core/GameObject.ts";
 
 describe("Collider", (): void => {
   const gjkCollisionHandler = new GjkCollisionHandler();
+
   /**
    * Tests two box that touch each other on one face trigger a collision.
    */
@@ -41,5 +42,41 @@ describe("Collider", (): void => {
 
     // Then
     expect(collision).toBeInstanceOf(Collision);
+  });
+
+  /**
+   * Tests two box that are not colliding do not trigger a collision.
+   */
+  it("should not trigger a collision on boxes not touching each other", () => {
+    // Given
+    const vertices: Vector3[] = [
+      new Vector3(0, 0, 0),
+      new Vector3(3, 0, 0),
+      new Vector3(3, -3, 0),
+      new Vector3(0, -3, 0),
+      new Vector3(0, 0, 2),
+      new Vector3(3, 0, 2),
+      new Vector3(3, -3, 2),
+      new Vector3(0, -3, 2),
+    ];
+    const object1: GameObject = new GameObject();
+    const box1: PolygonCollider = new PolygonCollider(vertices);
+    object1.addBehavior(box1);
+
+    const object2: GameObject = new GameObject();
+    const box2: PolygonCollider = new PolygonCollider(vertices);
+    object2.addBehavior(box2);
+    object2.transform.position.set(0, -1, -3);
+
+    expect(box1.getVerticesWithTransform()[7]).toEqual(new Vector3(0, -3, 2));
+    expect(box1.getGravitationCenter()).toEqual(new Vector3(1.5, -1.5, 1));
+    expect(box2.getVerticesWithTransform()[7]).toEqual(new Vector3(0, -4, -1));
+    expect(box2.getGravitationCenter()).toEqual(new Vector3(1.5, -1.5, 1));
+
+    // When
+    const collision = gjkCollisionHandler.areColliding(box1, box2);
+
+    // Then
+    expect(collision).toEqual(null);
   });
 });

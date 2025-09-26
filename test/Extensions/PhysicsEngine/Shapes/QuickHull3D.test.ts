8974fd8d669b91c00436f36de5ffc7cb4e7188ee
cyprien-png <cyprien@macbook-air-de-cyprien.home>
2025-09-26 15:55:51 +0200

    test: add a test case to ensure that QuickHull3D works with pyramid

--- test/Extensions/PhysicsEngine/Shapes/QuickHull3D.test.ts
+++ test/Extensions/PhysicsEngine/Shapes/QuickHull3D.test.ts
@@ -3,7 +3,7 @@ import { Vector3 } from "@core/MathStructures/Vector3";
 import { QuickHull3D } from "@extensions/PhysicsEngine/Shapes/QuickHull3D.ts";
 import { Face } from "@extensions/PhysicsEngine/Shapes/Face.ts";
 
-describe("Collider", (): void => {
+describe("QuickHull3D", (): void => {
   /**
    * Tests if we find the correct faces of a box.
    */
@@ -34,15 +34,53 @@ describe("Collider", (): void => {
       facesIndexes.push(face.indices);
     });
 
-    expect(facesIndexes).toEqual(
-      expect.arrayContaining([
-        expect.arrayContaining([0, 1, 2, 3]),
-        expect.arrayContaining([1, 2, 6, 5]),
-        expect.arrayContaining([0, 4, 7, 3]),
-        expect.arrayContaining([1, 0, 3, 2]),
-        expect.arrayContaining([4, 5, 6, 7]),
-        expect.arrayContaining([2, 3, 7, 6]),
-      ]),
-    );
+    const expectedFaces = [
+      [0, 1, 5, 4],
+      [1, 2, 6, 5],
+      [0, 4, 7, 3],
+      [1, 0, 3, 2],
+      [4, 5, 6, 7],
+      [2, 3, 7, 6],
+    ];
+
+    expect(facesIndexes).toEqual(expect.arrayContaining(expectedFaces));
+    expect(facesIndexes).toHaveLength(expectedFaces.length);
+  });
+
+  /**
+   * Tests if we find the correct faces of a pyramid.
+   */
+  it("should find the correct faces of a pyramid", () => {
+    // Given
+    const vertices: Vector3[] = [
+      new Vector3(0, 0, 0),
+      new Vector3(4, 0, 0),
+      new Vector3(3, 3, 0),
+      new Vector3(1, 1, 4),
+    ];
+
+    const hull = new QuickHull3D(vertices);
+    hull.build();
+
+    // When
+    const faces = hull.getMergedCoplanarFaces();
+
+    // Then
+    expect(faces).toHaveLength(4);
+
+    const facesIndexes: number[][] = [];
+    faces.forEach((face: Face) => {
+      facesIndexes.push(face.indices);
+    });
+
+    const expectedFaces = [
+      [0, 1, 2],
+      [0, 3, 1],
+      [0, 2, 3],
+      [1, 3, 2],
+    ];
+
+    expect(facesIndexes).toEqual(expect.arrayContaining(expectedFaces));
+    expect(facesIndexes).toHaveLength(expectedFaces.length);
   });
 });

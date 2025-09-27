38e173b3a926aa303f876378ea91d562b79264c9
Cyprien-png <cyprien04@hotmail.com>
2025-09-27 21:08:07 +0200

    test: add bunch of new tests casess on the GJK algorithm

--- test/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.test.ts
+++ test/Extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.test.ts
@@ -2,15 +2,17 @@ import { describe, it, expect } from "vitest";
 import { Vector3 } from "@core/MathStructures/Vector3";
 import { GjkCollisionHandler } from "@extensions/PhysicsEngine/CollisionHandlers/GjkCollisionHandler.ts";
 import { PolygonCollider } from "@extensions/PhysicsEngine/Colliders/PolygonCollider.ts";
+import { Collider } from "@extensions/PhysicsEngine/Colliders/Collider.ts";
 import { Collision } from "@extensions/PhysicsEngine/Colliders/Collision.ts";
 import { GameObject } from "@core/GameObject.ts";
+import { SinglePoint } from "@extensions/PhysicsEngine/Shapes/SinglePoint.ts";
 
 describe("Collider", (): void => {
   const gjkCollisionHandler = new GjkCollisionHandler();
 
-  // /**
-  //  * Tests two box that touch each other on one face trigger a collision.
-  //  */
+  /**
+   * Tests two box that touch each other on one face trigger a collision.
+   */
   it("should trigger a collision on boxes touching each other", () => {
     // Given
     const vertices: Vector3[] = [
@@ -81,39 +83,103 @@ describe("Collider", (): void => {
   });
 
   /**
-   * Tests a box and a pyramid that are not colliding do not trigger a collision.
+   * Tests a box and a point that are colliding trigger a collision.
    */
-  it("should not trigger a collision on box and pyramid not touching each other", () => {
+  it("should trigger a collision on box and point touching each other", () => {
     // Given
     const boxVertices: Vector3[] = [
       new Vector3(0, 0, 0),
       new Vector3(2, 0, 0),
-      new Vector3(2, 2, 0),
       new Vector3(0, 2, 0),
       new Vector3(0, 0, 2),
-      new Vector3(2, 0, 2),
-      new Vector3(2, 2, 2),
-      new Vector3(0, 2, 2),
     ];
     const object1: GameObject = new GameObject();
     const box1: PolygonCollider = new PolygonCollider(boxVertices);
     object1.addBehavior(box1);
 
-    const pyramidVertices: Vector3[] = [
-      new Vector3(0, 0, 0),
-      new Vector3(1, 0, 0),
-      new Vector3(0, 1, 0),
-      new Vector3(1, 1, 0),
-      new Vector3(0.5, 0.5, 2),
-    ];
+    const singlePoint = new SinglePoint();
+    const singlePointCollider = new Collider();
+    singlePointCollider.shape = singlePoint;
 
     const object2: GameObject = new GameObject();
-    const box2: PolygonCollider = new PolygonCollider(pyramidVertices);
-    object2.addBehavior(box2);
-    object2.transform.position.set(2.1, 2.1, 2.1);
+    object2.addBehavior(singlePointCollider);
 
     // When
-    const collision = gjkCollisionHandler.areColliding(box1, box2);
+    const collision = gjkCollisionHandler.areColliding(
+      singlePointCollider,
+      box1,
+    );
+
+    // Then
+    expect(collision).toBeInstanceOf(Collision);
+  });
+
+  /**
+   * Tests a box and a point that are not colliding do not trigger a collision.
+   */
+  it("should not trigger a collision on box and point not touching each other", () => {
+    // Given
+    const boxVertices: Vector3[] = [
+      new Vector3(0, 0, 0),
+      new Vector3(2, 0, 0),
+      new Vector3(0, 2, 0),
+      new Vector3(0, 0, 2),
+    ];
+    const object1: GameObject = new GameObject();
+    const box1: PolygonCollider = new PolygonCollider(boxVertices);
+    object1.addBehavior(box1);
+    object1.transform.position.set(-1, -1, -1);
+
+    const singlePoint = new SinglePoint();
+    const singlePointCollider = new Collider();
+    singlePointCollider.shape = singlePoint;
+
+    const object2: GameObject = new GameObject();
+    object2.addBehavior(singlePointCollider);
+
+    // When
+    const collision = gjkCollisionHandler.areColliding(
+      singlePointCollider,
+      box1,
+    );
+
+    // Then
+    expect(collision).toBeNull();
+  });
+
+  /**
+   * Tests tow big polyhedron that are not colliding do not trigger a collision.
+   */
+  it("should not trigger a collision on close polyhedrons with strong gravity center and point not touching each other", () => {
+    // Given
+    const vertices1: Vector3[] = [
+      new Vector3(0, 0, 0),
+      new Vector3(0, 90, 0),
+      new Vector3(0, 0, 90),
+      new Vector3(0, 90, 90),
+      new Vector3(0.1, 50, 50),
+    ];
+    const object1: GameObject = new GameObject();
+    const polyhedron1: PolygonCollider = new PolygonCollider(vertices1);
+    object1.addBehavior(polyhedron1);
+
+    const vertices2: Vector3[] = [
+      new Vector3(0, 0, 0),
+      new Vector3(0, 90, 0),
+      new Vector3(0, 0, 90),
+      new Vector3(0, 90, 90),
+      new Vector3(-0.1, 50, 50),
+    ];
+    const object2: GameObject = new GameObject();
+    const polyhedron2: PolygonCollider = new PolygonCollider(vertices2);
+    object2.addBehavior(polyhedron2);
+    object2.transform.position.set(0.25, 0, 0);
+
+    // When
+    const collision = gjkCollisionHandler.areColliding(
+      polyhedron2,
+      polyhedron1,
+    );
 
     // Then
     expect(collision).toBeNull();
